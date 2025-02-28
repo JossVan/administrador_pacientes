97727bf1291cb67a0f0aa43d8af32993c55e9725
Vanessa Polanco <vanessapolanco.tech@gmail.com>
2025-02-27 21:04:40 -0600

    zustand state

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,6 +1,16 @@
+import { usePatientStore } from "../store/store"
 
 export default function PatientsList() {
+  const patients = usePatientStore(state => state.patients);
+  console.log(patients);
   return (
-    <div>PatientsList</div>
+  <>
+    <h2 className="font-black text-3xl text-center">Pacientes</h2>
+
+    <p className="text-lg mt-5 text-center mb-10">
+      Administra tus {""}
+      <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+    </p>
+  </>
   )
 }
---
+++ src/store/store.ts
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+import { DraftPatient, Patient } from "../types";
+import { v4 as uuid } from 'uuid'
+type PatientState = {
+        patients: Patient[],
+        addPatient: (data: DraftPatient) => void
+}
+
+const createPatient = (patient: DraftPatient): Patient => ({ ...patient, id: uuid() })
+export const usePatientStore = create<PatientState>((set) => ({
+        patients: [],
+        addPatient: (data) => {
+                set((state) => ({
+                        patients: [...state.patients, createPatient(data)]
+                }))
+        }
+}))
